69e98bc82f670844c8b31b018852838185dd71df
Amit Sharafi <amitsharafi9@gmail.com>
2024-08-19 15:14:45 +0300

    changed text

--- api/chat/chat.routes.js
+++ api/chat/chat.routes.js
@@ -1,16 +1,7 @@
 const express = require('express')
-const {
-  requireAuth,
-  requireAdmin,
-} = require('../../middlewares/requireAuth.middleware')
+const { requireAuth, requireAdmin } = require('../../middlewares/requireAuth.middleware')
 const { log } = require('../../middlewares/logger.middleware')
-const {
-  addMsg,
-  addChat,
-  getChats,
-  deleteChat,
-  updateChat,
-} = require('./chat.controller')
+const { addMsg, addChat, getChats, deleteChat, updateChat } = require('./chat.controller')
 const router = express.Router()
 
 // middleware that is specific to this router
@@ -19,7 +10,7 @@ const router = express.Router()
 router.get('/', log, getChats)
 router.post('/', log, requireAuth, addChat)
 router.put('/', log, updateChat)
-router.put('/msg', addMsg)
+router.put('/msg', requireAuth, addMsg)
 router.delete('/:id', requireAuth, deleteChat)
 
 module.exports = router
